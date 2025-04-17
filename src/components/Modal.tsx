3eba6279819368a1742985a57d466f094e29f250
Martin <115161917+Martin13025@users.noreply.github.com>
2025-04-17 12:19:35 -0300

    Update Modal.tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,1 +1,27 @@
+import React from "react";
 
+interface ModalProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  title: string;
+  content: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  closeModal,
+  title,
+  content,
+}) => {
+  return (
+    <div className={`modal-overlay ${isOpen ? "open" : ""}`}>
+      <div className="modal-content">
+        <div className="modal-close-bar" onClick={closeModal}></div>
+        <h3>{title}</h3>
+        <div>{content}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
