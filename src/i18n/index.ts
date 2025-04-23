9eeeb785220c1a0e70a5b1d278f0f3a7a871e54a
Martin <115161917+Martin13025@users.noreply.github.com>
2025-04-23 11:54:23 -0300

    Update index.ts

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,40 +1,63 @@
-export const translations = {
-  en: {
-    title: "crawlLab",
-    subheading: "Look for everything MORE you need",
-    placeholder: "Enter keywords (separated by commas)",
-    placetext: "Enter keywords",
-    parseButton: "Parse",
-    results: "Results:",
-    noLinks: "No links found",
-    backButton: "Back to Search",
-    modalTitle: "About this service",
-    modalContent:
-      "CrawlLab is a tool designed to simplify your SEO analysis and improve your online visibility. It helps you discover relevant links based on keywords you provide, making it an essential tool for SEO specialists, content creators, and digital marketers.",
-    supportTitle: "Support the developer",
-    supportContent:
-      "If you find CrawlLab useful and want to support the development of this tool, you can make a donation. Your support helps improve and expand this project, allowing us to add more features.",
-    connectTitle: "Connect with the developer",
-    aggressiveModeText: "Look for everything MORE you need",
-    normalModeText: "Look for everything you need",
-  },
-  es: {
-    title: "crawlLab",
-    subheading: "Busca todo lo que MÁS necesitas",
-    placeholder: "Ingresa palabras clave (separadas por comas)",
-    placetext: "Ingresa palabras",
-    parseButton: "Parsear",
-    results: "Resultados:",
-    noLinks: "No se encontraron enlaces",
-    backButton: "Volver a la búsqueda",
-    modalTitle: "Sobre este servicio",
-    modalContent:
-      "CrawlLab es una herramienta diseñada para simplificar tu análisis SEO y mejorar tu visibilidad en línea. Te ayuda a descubrir enlaces relevantes según las palabras clave que proporcionas, siendo una herramienta esencial para especialistas SEO, creadores de contenido y comercializadores digitales.",
-    supportTitle: "Apoya al desarrollador",
-    supportContent:
-      "Si encuentras útil CrawlLab y deseas apoyar el desarrollo de esta herramienta, puedes hacer una donación. Tu apoyo ayuda a mejorar y expandir este proyecto, permitiéndonos agregar más funciones.",
-    connectTitle: "Conéctate con el desarrollador",
-    aggressiveModeText: "Busca todo lo que MÁS necesitas",
-    normalModeText: "Busca todo lo que necesitas",
-  },
-};
+type Translation = {
+    title: string;
+    subheading: string;
+    placeholder: string;
+    placetext: string;
+    parseButton: string;
+    results: string;
+    noLinks: string;
+    backButton: string;
+    modalTitle: string;
+    modalContent: string;
+    supportTitle: string;
+    supportContent: string;
+    connectTitle: string;
+    aggressiveModeText: string;
+    normalModeText: string;
+  };
+  
+  type Translations = {
+    en: Translation;
+    es: Translation;
+  };
+  
+  export const translations: Translations = {
+    en: {
+      title: "crawlLab",
+      subheading: "Look for everything MORE you need",
+      placeholder: "Enter keywords (separated by commas)",
+      placetext: "Enter keywords",
+      parseButton: "Parse",
+      results: "Results:",
+      noLinks: "No links found",
+      backButton: "Back to Search",
+      modalTitle: "About this service",
+      modalContent:
+        "CrawlLab is a tool designed to simplify your SEO analysis and improve your online visibility. It helps you discover relevant links based on keywords you provide, making it an essential tool for SEO specialists, content creators, and digital marketers.",
+      supportTitle: "Support the developer",
+      supportContent:
+        "If you find CrawlLab useful and want to support the development of this tool, you can make a donation. Your support helps improve and expand this project, allowing us to add more features.",
+      connectTitle: "Connect with the developer",
+      aggressiveModeText: "Look for everything MORE you need",
+      normalModeText: "Look for everything you need",
+    },
+    es: {
+      title: "crawlLab",
+      subheading: "Busca todo lo que MÁS necesitas",
+      placeholder: "Ingresa palabras clave (separadas por comas)",
+      placetext: "Ingresa palabras",
+      parseButton: "Parsear",
+      results: "Resultados:",
+      noLinks: "No se encontraron enlaces",
+      backButton: "Volver a la búsqueda",
+      modalTitle: "Sobre este servicio",
+      modalContent:
+        "CrawlLab es una herramienta diseñada para simplificar tu análisis SEO y mejorar tu visibilidad en línea. Te ayuda a descubrir enlaces relevantes según las palabras clave que proporcionas, siendo una herramienta esencial para especialistas SEO, creadores de contenido y comercializadores digitales.",
+      supportTitle: "Apoya al desarrollador",
+      supportContent:
+        "Si encuentras útil CrawlLab y deseas apoyar el desarrollo de esta herramienta, puedes hacer una donación. Tu apoyo ayuda a mejorar y expandir este proyecto, permitiéndonos agregar más funciones.",
+      connectTitle: "Conéctate con el desarrollador",
+      aggressiveModeText: "Busca todo lo que MÁS necesitas",
+      normalModeText: "Busca todo lo que necesitas",
+    },
+  };
