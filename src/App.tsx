eee037c26e0771dc21788e95b3c393f2bc9d9a49
Martin <115161917+Martin13025@users.noreply.github.com>
2025-05-21 23:20:20 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -123,7 +123,7 @@ const App: React.FC = () => {
       </button>
 
       <div className="footer">
-        <div className="footer-left">v 1.4.0</div>
+        <div className="footer-left">v 1.4.1</div>
         <div className="footer-center">
           All rights reserved. Developer: Martin Daniels.
         </div>
